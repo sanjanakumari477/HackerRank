dfa030d62ddea3f8666df80e1eecb90636282be3
Sanjana Kumari <sksanjana1502@gmail.com>
2025-11-26 23:47:27 +0530

    Create Manhattam_Distance.sql

---
+++ Manhattam_Distance.sql
@@ -0,0 +1,17 @@
+Consider P1 and P2 to be two points on a 2D plane.
+
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+Query the Manhattan Distance between points p1 and p2 and round it to a scale of 4 decimal places.
+
+Sol:--
+SELECT 
+  ROUND(
+    ABS(MAX(LONG_W * 1.0) - MIN(LONG_W * 1.0)) + 
+    ABS(MAX(LAT_N * 1.0) - MIN(LAT_N * 1.0)),
+    4
+  ) AS manhattan_distance
+FROM STATION
+WHERE LAT_N IS NOT NULL AND LONG_W IS NOT NULL;
