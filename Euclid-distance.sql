18b091c5c2136894ae9a89dac6c9de39e0867732
Sanjana Kumari <sksanjana1502@gmail.com>
2025-12-03 18:57:05 +0530

    Update and rename euclid-distance.sql to Euclid-distance.sql

--- Euclid-distance.sql
+++ Euclid-distance.sql
@@ -7,7 +7,6 @@ Ans : SELECT
         SQRT(
             POWER(MAX(LAT_N) - MIN(LAT_N), 2) +
             POWER(MAX(LONG_W) - MIN(LONG_W), 2)
-        ),
-        4
+        ), 4
     ) AS distance
 FROM STATION;
