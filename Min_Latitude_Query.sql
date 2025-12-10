429a7a93248ba630c0f6894f6d51573229d4b759
Sanjana Kumari <sksanjana1502@gmail.com>
2025-12-10 14:33:42 +0530

    Update Min_Latitude_Query.sql

--- Min_Latitude_Query.sql
+++ Min_Latitude_Query.sql
@@ -1,4 +1,4 @@
-Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than .
+Que : Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in STATION is greater than .
 Round your answer to  decimal places.
 
 Sol: SELECT ROUND (LONG_W,4)
