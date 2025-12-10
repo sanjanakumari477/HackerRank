2c7449cf6b9376c6223c8b28d2af213ceb249a58
Sanjana Kumari <sksanjana1502@gmail.com>
2025-12-10 14:31:58 +0530

    Create hackerrank_euclidean_distance.sql

---
+++ hackerrank_euclidean_distance.sql
@@ -0,0 +1,12 @@
+Que: Consider p1(a,c)and p2(b,d) to be two points on a 2D plane where(a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) and(c,d)are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+
+    Query the Euclidean Distance between points p1  and p2 and format your answer to display 4 decimal digits.
+
+Ans : SELECT DECIMAL(
+       SQRT(
+        POWER(MAX(LAT_N) - MIN(LAT_N), 2) +
+        POWER(MAX(LONG_W) - MIN(LONG_W), 2)
+        ),
+       10, 4
+             ) AS DISTANCE
+                 FROM STATION;
