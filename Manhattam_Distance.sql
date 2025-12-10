7ac4ee7e82386bc46f88a792d3690fc516431cab
Sanjana Kumari <sksanjana1502@gmail.com>
2025-12-10 14:33:03 +0530

    Update Manhattam_Distance.sql

--- Manhattam_Distance.sql
+++ Manhattam_Distance.sql
@@ -1,10 +1,10 @@
-Consider P1 and P2 to be two points on a 2D plane.
+Que : Consider P1 and P2 to be two points on a 2D plane.
 
-a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
-b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
-c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
-d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
-Query the Manhattan Distance between points p1 and p2 and round it to a scale of 4 decimal places.
+      a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+      b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+      c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+      d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+      Query the Manhattan Distance between points p1 and p2 and round it to a scale of 4 decimal places.
 
 Sol:--
 SELECT 
