fe7fbe308ff3ab2d20e40feb9e88da43ede53799
Sanjana Kumari <sksanjana1502@gmail.com>
2025-12-18 11:06:10 +0530

    Create africa_city_names.sql

---
+++ africa_city_names.sql
@@ -0,0 +1,10 @@
+Ques: Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+
+      Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+
+Ans: SELECT CITY.NAME
+    FROM CITY
+        JOIN COUNTRY
+        ON CITY.COUNTRYCODE = COUNTRY.CODE
+        WHERE COUNTRY.CONTINENT = 'Africa';
