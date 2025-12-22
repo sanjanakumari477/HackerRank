776ac2b412eb8abab3dcc9284feccc80a0d0ffdf
Sanjana Kumari <sksanjana1502@gmail.com>
2025-12-22 08:57:35 +0530

    Update Population_Census_b.join.sql

--- Population_Census_b.join.sql
+++ Population_Census_b.join.sql
@@ -1,9 +1,9 @@
-                                              STARTING JOIN SOLUTION ON HEAKER'S RANK 
+                        ---                      STARTING JOIN SOLUTION ON HEAKER'S RANK 
                                           =================================================
                                                 
-Ques: Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+---Ques: Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
 
-      Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+   ---   Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
 Ans: SELECT SUM(C.POPULATION)
      FROM CITY C   
