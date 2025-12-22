3db0569fc39195a6d1516a63933e293f3f3780ad
Sanjana Kumari <sksanjana1502@gmail.com>
2025-12-22 08:58:18 +0530

    Update continent_wise_avg_population.sql

--- continent_wise_avg_population.sql
+++ continent_wise_avg_population.sql
@@ -1,7 +1,7 @@
-Ques: Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and 
+'''Ques: Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and 
       their respective average city populations (CITY.Population) rounded down to the nearest integer.
 
-      Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+      Note: CITY.CountryCode and COUNTRY.Code are matching key columns.'''
 
 Ans: SELECT 
     CO.CONTINENT,
