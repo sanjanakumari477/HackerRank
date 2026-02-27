1ff7d3d5cc1b7e09d7b9ea3eed34450381f6410d
Sanjana Kumari <sksanjana1502@gmail.com>
2026-02-27 23:52:43 +0530

    Update Africa_city_names.sql

--- Africa_city_names.sql
+++ Africa_city_names.sql
@@ -1,4 +1,4 @@
-'''Ques: Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+' ''Ques: Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
 
        Note: CITY.CountryCode and COUNTRY.Code are matching key columns.'''
 
