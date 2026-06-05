580ba4c0072a52e89a7e3c39ca593605e35d9d3a
Sanjana Kumari <sksanjana1502@gmail.com>
2026-06-05 23:55:00 +0530

    Update Contest_leaderboard.sql

--- Contest_leaderboard.sql
+++ Contest_leaderboard.sql
@@ -4,6 +4,7 @@ Ques : You did such a great job helping Julia with her last coding contest chall
     and total score of the hackers ordered by the descending score. If more than one hacker achieved the same total score, then sort the result by ascending hacker_id. 
     Exclude all hackers with a total score of  from your result.
 
+
 Ans : SELECT 
     h.hacker_id,
     h.name,
